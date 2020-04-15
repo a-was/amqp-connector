1e2a2625277654f5eec354fb0d232f2205b30f0b
Adam Wasilewski <chaper122@gmail.com>
2020-04-15 12:31:56 +0200

    new file

---
+++ class.js
@@ -0,0 +1,77 @@
+const amqp = require('amqplib')
+
+class AMQPConnector {
+    constructor(uri, queue) {
+        this.uri = uri
+        this.queue = queue
+        this.connected = false
+
+        amqp.connect(uri)
+            .then((conn) => {
+                this.connection = conn
+                return this.connection.createChannel()
+            })
+            .then((ch) => {
+                this.channel = ch
+                this.channel.assertQueue(this.queue, { durable: true })
+            })
+            .then(() => {
+                this.connected = true
+            })
+            .catch(function (err) {
+                console.error(err)
+            })
+    }
+
+    _checkReady() {
+        return new Promise((resolve, reject) => {
+            var start = Date.now()
+            var interval = setInterval(() => {
+                if (this.connected === true) {
+                    clearInterval(interval)
+                    resolve()
+                } else if (Date.now() - start > 5000) {
+                    reject(new Error('Connection timed out'))
+                }
+            }, 20)
+        })
+    }
+
+    _defaultCallback(message) { console.log(message) }
+
+    receive(callback = this._defaultCallback) {
+        this._checkReady().then(() => {
+            try {
+                this.channel.consume(this.queue, (message) => {
+                    if (message != null) {
+                        callback(message.content.toString())
+                        this.channel.ack(message)
+                    }
+                }, { noAck: false })
+            } catch (error) {
+                console.error(error)
+            }
+        })
+    }
+
+    send(message) {
+        this._checkReady().then(() => {
+            try {
+                this.channel.sendToQueue(this.queue, Buffer.from(message), { persistent: true })
+            } catch (error) {
+                console.error(error)
+            }
+        })
+    }
+
+    close() {
+        setTimeout(() => {
+            this._checkReady().then(() => {
+                this.channel.close()
+                this.connection.close()
+            })
+        }, 200)
+    }
+}
+
+module.exports.AMQPConnector = AMQPConnector
