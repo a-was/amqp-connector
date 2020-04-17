b5bb56f59056e18ad980a1cb44e7fb63aaa1612f
Adam Wasilewski <chaper122@gmail.com>
2020-04-17 11:33:40 +0200

    ns fix

--- index.d.ts
+++ index.d.ts
@@ -6,11 +6,4 @@ interface AMQPConnector {
 
 declare function amqpConnector(uri: string, queue: string): AMQPConnector
 
-declare namespace amqpConnector {
-    function _checkReady(): void
-    function _defaultCallback(message: string): void
-    export function receive(callback: (message: string) => void): void
-    export function send(message: string): void
-    export function close(): void
-}
 export = amqpConnector
